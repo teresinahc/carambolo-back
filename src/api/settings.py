2f67df775b3a18542cde708a9354e4fb9515cfa4
caiovictormc <caio@retake.com.br>
2019-09-07 14:53:56 -0300

    Create app common

--- src/api/settings.py
+++ src/api/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    "apps.common"
+]
+
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS = (
+    DEFAULT_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
