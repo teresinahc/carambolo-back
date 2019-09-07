4fcecc503aee5a61effc3043676030712906c220
mandala21 <lucasresone@gmail.com>
2019-09-07 15:18:34 -0300

    add env.exemple e config do banco settings

--- src/api/settings.py
+++ src/api/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
