792e5bfb65cca3c66e810429d4bed6263a5a6ea2
mandala21 <lucasresone@gmail.com>
2019-09-07 16:23:39 -0300

    add model mood

--- src/apps/moods/models.py
+++ src/apps/moods/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Mood(models.Model):
-    feeling = models.IntegerField()
+    feeling = models.IntegerField(null=True,blank=True)
+    thought = models.TextField(null=True,blank=True)
+    racional_thought = models.TextField(null=True,blank=True)
+    day = models.DateField(null=True,blank=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now().date())
+    updated_at = models.DateTimeField(auto_now=True, null=True,blank=True)
+    #relationships
+    # user = models.ForeignKey()
